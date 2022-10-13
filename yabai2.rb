c7fe500a77911b0e31fb64d33d8fbc3049b91fc5
Tushar K <kohli.tushar18@gmail.com>
2022-10-13 10:27:33 -0500

    v1

--- yabai2.rb
+++ yabai2.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Yabai2 < Formula
   desc "A tiling window manager for macOS based on binary space partitioning"
-  homepage ""
+  homepage "https://github.com/Tushar1729-k/yabai2"
   url "https://github.com/Tushar1729-k/yabai2/archive/refs/tags/v1.tar.gz"
   sha256 "bffb2cf29f0f7aff45a0eb8cff97479d1c5562ac51e8e5c046db96c1567be5e7"
   license "MIT"
@@ -16,6 +16,7 @@ class Yabai2 < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install 'yabai2'
   end
 
   test do
